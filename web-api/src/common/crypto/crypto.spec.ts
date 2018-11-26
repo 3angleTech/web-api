eea851ff8a23a64a756e2d638e4e6dd1cce025c7
Ionut Cristian Paraschiv <ionut@3angle.tech>
2018-11-27 00:02:08 +0200

    #1 Added tests

---
+++ web-api/src/common/crypto/crypto.spec.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright (c) 2018 THREEANGLE SOFTWARE SOLUTIONS SRL
+ * Available under MIT license webFrame/LICENSE
+ */
+
+import { expect } from 'chai';
+import { encrypt, verify } from './crypto';
+
+describe('Crypto Service', () => {
+  it('Should encrypt and verify a string', () => {
+    const original = 'test132_21';
+    const encrypted = encrypt(original);
+    expect(encrypted).to.not.equal(original, 'Encrypted text should not be equal with the original');
+
+    const result = verify(original, encrypted);
+    expect(result).to.equal(true, 'Verify function should return true for the encrypted text.');
+  });
+});
