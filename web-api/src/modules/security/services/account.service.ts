7c25185095bcaf3f0d458f6d20a3b057d08eda8c
Gabi Gutu <gabi.gutu@gmail.com>
2019-03-29 22:40:05 +0200

    #8 Added user create method with e-mail verification

--- web-api/src/modules/security/services/account.service.ts
+++ web-api/src/modules/security/services/account.service.ts
@@ -6,17 +6,21 @@
 
 import { inject, injectable } from 'inversify';
 import { isNil } from 'lodash';
+import { IConfigurationService } from '../../../common/configuration';
 import { verify } from '../../../common/crypto';
 import { IEmailService } from '../../../common/email';
 import { IJsonConverterService } from '../../../common/json-converter';
 import { DatabaseModel, IDatabaseContext, User } from '../../../data';
 import { Credentials, IAccountService } from './account.service.interface';
+import { IOAuthServerModel } from './oauth-server-model.interface';
 
 @injectable()
 export class AccountService implements IAccountService {
   constructor(
     @inject(IDatabaseContext) private dbContext: IDatabaseContext,
     @inject(IJsonConverterService) private jsonConverter: IJsonConverterService,
+    @inject(IConfigurationService) private configuration: IConfigurationService,
+    @inject(IOAuthServerModel) private oauthServerModel: IOAuthServerModel,
     @inject(IEmailService) private emailService: IEmailService,
   ) { }
 
@@ -47,6 +51,32 @@ export class AccountService implements IAccountService {
       throw new Error('Account not found');
     }
     return this.jsonConverter.deserialize(userObject, User);
+  }
+
+  public async create(user: User): Promise<User> {
+    const userObject = await this.dbContext.getModel(DatabaseModel.Users).create(user);
+    if (isNil(userObject)) {
+      throw new Error('Account not created');
+    }
+    const token = await this.generateToken(user);
+    const params = {
+      to: user.email,
+      from: this.configuration.getEmailParams().from,
+      subject: '',
+      rawText: '',
+      htmlText: '',
+      token: token,
+    };
+    this.emailService.sendAccountActivationEmail(params);
+    return this.jsonConverter.deserialize(userObject, User);
+  }
+
+  private async generateToken(user: User): Promise<string> {
+    return this.oauthServerModel.generateAccessToken(
+      this.configuration.getOAuthConfig().clients[0],
+      user,
+      '',
+    );
   }
 
 }
