b22aef7e3185df3a13e19a975fbe1a55d3a0d018
Gabi Gutu <gabi.gutu@gmail.com>
2019-03-29 22:38:56 +0200

    #8 Added missing files for email provider

--- web-api/src/common/email/index.ts
+++ web-api/src/common/email/index.ts
@@ -5,12 +5,19 @@
  */
 
 import { Container } from 'inversify';
-import { MockEmailService } from './email.service';
+import { IConfigurationService } from '../configuration';
+import { EmailProvider } from './email-provider.enum';
 import { IEmailService } from './email.service.interface';
+import { MockEmailService } from './mock-email.service';
 import { SendGridService } from './sendgrid.service';
 
 export function bindDependencies(container: Container): void {
-    container.bind<IEmailService>(IEmailService).to(SendGridService);
-    container.bind<IEmailService>(IEmailService).to(MockEmailService);
+    const configurationService = container.get<IConfigurationService>(IConfigurationService);
+    const emailProvider = configurationService.getEmailProvider();
+    if (emailProvider === EmailProvider.SendGrid) {
+        container.bind<IEmailService>(IEmailService).to(SendGridService);
+    } else {
+        container.bind<IEmailService>(IEmailService).to(MockEmailService);
+    }
 }
 export * from './email.service.interface';
---
+++ web-api/src/common/email/mock-email.service.ts
@@ -0,0 +1,41 @@
+/**
+ * @license
+ * Copyright (c) 2018 THREEANGLE SOFTWARE SOLUTIONS SRL
+ * Available under MIT license webApi/LICENSE
+ */
+
+import { injectable } from 'inversify';
+import { ActivateAccountParams } from '../../data/data-objects/email/activate-account-params.do';
+import { EmailParams } from '../../data/data-objects/email/email-params.do';
+import { NewAccountParams } from '../../data/data-objects/email/new-account-params.do';
+import { Logger, LogLevel } from '../logger';
+import { IEmailService } from './email.service.interface';
+
+@injectable()
+export class MockEmailService implements IEmailService {
+
+    private static instance: MockEmailService;
+
+    constructor() {
+        if (MockEmailService.instance) {
+            return MockEmailService.instance;
+        }
+        MockEmailService.instance = this;
+    }
+
+    public async sendAccountActivationEmail(params: ActivateAccountParams): Promise<void> {
+        this.sendEmail(params);
+        return Promise.resolve();
+    }
+
+    public sendNewAccountEmail(params: NewAccountParams): Promise<void> {
+        this.sendEmail(params);
+        return Promise.resolve();
+    }
+
+    public sendEmail(params: EmailParams): Promise<void> {
+        Logger.getInstance().log(LogLevel.Debug, 'Sending e-mail...');
+        return Promise.resolve();
+    }
+
+}
