0089581b580e0eda17a361a176d8c62995695cd4
Catalin Andrei Dobre <catalin.dobre@3angle.tech>
2019-04-10 00:05:29 +0300

    #8 added script utils

---
+++ web-api/src/common/utils/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright (c) 2018 THREEANGLE SOFTWARE SOLUTIONS SRL
+ * Available under MIT license webFrame/LICENSE
+ */
+
+/**
+ * Provides utility functions.
+ *
+ * This file contains an explicit list of things exported in the current directory.
+ */
+
+export { isNil } from './is-nil';
+export { tryGetValue } from './try-get-value';
