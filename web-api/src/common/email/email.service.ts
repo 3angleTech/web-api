86fec3d420bd416622b37e86b1f0485b8892d295
Catalin Andrei Dobre <catalin.dobre@3angle.tech>
2019-04-10 16:21:39 +0300

    #8 cleanup emailService

--- web-api/src/common/email/email.service.ts
+++ web-api/src/common/email/email.service.ts
@@ -5,7 +5,7 @@
  */
 
 import { inject, injectable } from 'inversify';
-import { EmailTemplate, EmailTemplates, IConfigurationService } from '../configuration';
+import { IConfigurationService } from '../configuration';
 import { IStringTemplateService } from '../string-template';
 import { ActivateAccountParams, EmailParams, IEmailProviderDriver, IEmailService, NewAccountParams } from './email.service.interface';
 
@@ -18,18 +18,27 @@ export class EmailService implements IEmailService {
         @inject(IStringTemplateService) private templateService: IStringTemplateService,
     ) {
     }
+
     public async sendEmail(params: EmailParams): Promise<void> {
       this.emailDriver.sendEmail(params);
     }
 
     public async sendAccountActivationEmail(params: ActivateAccountParams): Promise<void> {
-        let template = this.getTemplate('activation');
+        const template = this.configuration.getEmailTemplates()['activation'];
         const parameters = {
           activationToken: params.token,
         };
-        template = this.compileTemplate(template, parameters);
-        let localParams = { ...params };
-        localParams = this.setTextParams(localParams, template);
+        const htmlContent = this.templateService.interpolate(template.html, parameters);
+        const textContent = this.templateService.interpolate(template.text, parameters);
+
+        const localParams: EmailParams = {
+          to: params.to,
+          from: params.from,
+          html: htmlContent,
+          text: textContent,
+          subject: params.subject,
+        };
+
         await this.sendEmail(localParams);
         return Promise.resolve();
     }
@@ -39,26 +48,4 @@ export class EmailService implements IEmailService {
         await this.sendEmail(params);
         return Promise.resolve();
     }
-
-    public getTemplate(key: string): EmailTemplate {
-      const emailTemplates: EmailTemplates = this.configuration.getEmailTemplates();
-      return emailTemplates[key];
-    }
-
-    public compileTemplate(template: EmailTemplate, parameters: Object): EmailTemplate {
-      const htmlContent = this.templateService.interpolate(template.html, parameters);
-      const textContent = this.templateService.interpolate(template.text, parameters);
-      return {
-          html: htmlContent,
-          text: textContent,
-          subject: template.subject,
-      };
-    }
-
-    public setTextParams<T extends EmailParams>(params: T, template: EmailTemplate): T {
-      params.text = template.text;
-      params.html = template.html;
-      params.subject = template.subject;
-      return params;
-  }
 }
