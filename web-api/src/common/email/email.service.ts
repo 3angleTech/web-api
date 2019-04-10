8e84a402989a72fdc0cbece1c490d60001a2b4df
Catalin Andrei Dobre <catalin.dobre@3angle.tech>
2019-04-10 19:28:30 +0300

    #8 added EmailBuilder

--- web-api/src/common/email/email.service.ts
+++ web-api/src/common/email/email.service.ts
@@ -5,9 +5,88 @@
  */
 
 import { inject, injectable } from 'inversify';
-import { IConfigurationService } from '../configuration';
-import { IStringTemplateService } from '../string-template';
+import { EmailTemplate, IConfigurationService } from '../configuration';
+import { IStringTemplateService, StringTemplateService } from '../string-template';
+import { isNil, tryGetValue } from '../utils';
 import { ActivateAccountParams, EmailParams, IEmailProviderDriver, IEmailService, NewAccountParams } from './email.service.interface';
+
+interface Email {
+  to: string;
+  from: string;
+  subject: string;
+  html: string;
+  text: string;
+}
+
+interface EmailTemplateVariables {
+  subject?: Object;
+  html?: Object;
+  text?: Object;
+}
+
+class EmailBuilder {
+  private _to: string;
+  private _from: string;
+  private _html: string;
+  private _text: string;
+  private _subject: string;
+
+  private templateService: IStringTemplateService;
+
+  constructor() {
+    this.templateService = new StringTemplateService();
+  }
+
+  public set to(to: string) {
+    this._to = to;
+  }
+
+  public set from(from: string) {
+    this._from = from;
+  }
+
+  public set html(html: string) {
+    this._html = html;
+  }
+
+  public set text(text: string) {
+    this._text = text;
+  }
+
+  public set subject(subject: string) {
+    this._subject = subject;
+  }
+
+  public build(templateVariables: EmailTemplateVariables): Email {
+    const subjectVariables = tryGetValue(templateVariables, 'subject');
+    const htmlVariables = tryGetValue(templateVariables, 'html');
+    const textVariables = tryGetValue(templateVariables, 'text');
+
+    let subject = this._subject;
+    let html = this._html;
+    let text = this._text;
+
+    if (!isNil(subjectVariables)) {
+      subject = this.templateService.interpolate(subject, subjectVariables);
+    }
+
+    if (!isNil(htmlVariables)) {
+      html = this.templateService.interpolate(html, htmlVariables);
+    }
+
+    if (!isNil(textVariables)) {
+      text = this.templateService.interpolate(text, textVariables);
+    }
+
+    return {
+      to: this._to,
+      from: this._from,
+      subject: subject,
+      html: html,
+      text: text,
+    };
+  }
+}
 
 @injectable()
 export class EmailService implements IEmailService {
@@ -19,27 +98,30 @@ export class EmailService implements IEmailService {
     ) {
     }
 
-    public async sendEmail(params: EmailParams): Promise<void> {
-      this.emailDriver.sendEmail(params);
+    public async sendEmail(email: Email): Promise<void> {
+      this.emailDriver.sendEmail(email);
     }
 
     public async sendAccountActivationEmail(params: ActivateAccountParams): Promise<void> {
         const template = this.configuration.getEmailTemplates()['activation'];
+
+        const emailBuilder = new EmailBuilder();
+        emailBuilder.to = params.to;
+        emailBuilder.from = params.from;
+        emailBuilder.subject = params.subject;
+        emailBuilder.html = template.html;
+        emailBuilder.text = template.text;
+
         const parameters = {
           activationToken: params.token,
         };
-        const htmlContent = this.templateService.interpolate(template.html, parameters);
-        const textContent = this.templateService.interpolate(template.text, parameters);
 
-        const localParams: EmailParams = {
-          to: params.to,
-          from: params.from,
-          html: htmlContent,
-          text: textContent,
-          subject: params.subject,
-        };
+        const email = emailBuilder.build({
+          html: parameters,
+          text: parameters,
+        });
 
-        await this.sendEmail(localParams);
+        await this.sendEmail(email);
         return Promise.resolve();
     }
 
